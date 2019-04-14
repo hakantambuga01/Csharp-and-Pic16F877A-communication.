9b826e1a4d545cd543899b3971cda2d09f9b7500
hakantambuga01 <hakan.tambuga01@gmail.com>
2019-04-14 14:38:25 +0300

    Hakan TAMBUGA

---
+++ Pic_DcStep1/Pic_DcStep1/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace Pic_DcStep1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "BAĞLANTI";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(149, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "PORT AÇ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(149, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 21);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "PORT KAPAT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Control;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(3, 105);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 21);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "PWM ARTTIR";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button3_MouseDown);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Control;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(3, 133);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 21);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "PWM AZALT";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button4_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SteelBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(5, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = " DC MOTOR ";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Control;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(2, 160);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(98, 21);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "SAĞA DÖN";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.Control;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(2, 187);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(98, 21);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "SOLA DÖN";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.Location = new System.Drawing.Point(2, 214);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(98, 21);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "MOTOR DUR";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.YellowGreen;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(146, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "STEP MOTOR";
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button8.Location = new System.Drawing.Point(143, 105);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(110, 21);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "HIZ ARTTIR";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            this.button8.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button8_MouseDown);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button9.Location = new System.Drawing.Point(143, 132);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(110, 21);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "HIZ AZALT";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            this.button9.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button9_MouseDown);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button10.Location = new System.Drawing.Point(143, 160);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(110, 21);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "SAĞA DÖN";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.SystemColors.Control;
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button11.Location = new System.Drawing.Point(143, 187);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(110, 21);
+            this.button11.TabIndex = 15;
+            this.button11.Text = "SOLA DÖN";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button12.Location = new System.Drawing.Point(143, 214);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(110, 21);
+            this.button12.TabIndex = 16;
+            this.button12.Text = "MOTOR DUR";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 242);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Hız Kademesi = ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(140, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Hız Kademesi = ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(218, 242);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aquamarine;
+            this.ClientSize = new System.Drawing.Size(280, 262);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DC-STEP MOTOR HIZ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Pic_DcStep1/Pic_DcStep1/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Pic_DcStep1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] portlar = SerialPort.GetPortNames();
+        int pwm;
+        int step;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (string port in portlar)
+            {
+                comboBox1.Items.Add(port);
+            }
+            label4.Text = "0";
+            label6.Text = "0";
+        }
+
+        //-----------------------> Port Aç <------------------------------
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(serialPort1.IsOpen==false)
+            {
+                if (comboBox1.Text == "")
+                    return;
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.BaudRate = Convert.ToInt32("9600");
+                try
+                {
+                    serialPort1.Open();
+                    button1.BackColor = Color.Green;
+                    button2.BackColor = Color.WhiteSmoke;
+                    serialPort1.DiscardOutBuffer();
+                    serialPort1.DiscardInBuffer();
+                }
+                catch (Exception hata)
+                {
+
+                    MessageBox.Show("Hata:" + hata.Message);
+                }
+             
+            }
+        }
+        //---------------------> Port Kapat <--------------------------------
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.Close();
+                button2.BackColor = Color.Red;
+                button1.BackColor = Color.WhiteSmoke;
+                label4.Text = "0";
+                pwm = 0;
+            }
+        }
+
+        //------------------> DC PWM ARTTIR <-----------------------------
+        private void button3_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("W");
+            button3.BackColor = Color.WhiteSmoke;
+            pwm = pwm + 10;
+            if (pwm >= 250) pwm = 250;
+            label4.Text = Convert.ToString(pwm);
+        }
+
+        //------------------> DC PWM AZALT <-----------------------------
+        private void button4_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("S");
+            button4.BackColor = Color.WhiteSmoke;
+            pwm = pwm - 10;
+            if (pwm <= 0) pwm = 0;
+            label4.Text = Convert.ToString(pwm);
+        }
+
+        //------------------> DC SAĞA DÖN <-----------------------------
+        private void button5_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("D");
+            button5.BackColor = Color.Red;
+            button6.BackColor = Color.WhiteSmoke;
+            label4.Text = "0";
+            pwm = 0;
+        }
+
+        //------------------> DC SOLA DÖN <-----------------------------
+        private void button6_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("A");
+            button6.BackColor = Color.Red;
+            button5.BackColor = Color.WhiteSmoke;
+            label4.Text = "0";
+            pwm = 0;
+        }
+
+        //------------------> DC STOP <-----------------------------
+        private void button7_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("Z");
+            button5.BackColor = Color.WhiteSmoke;
+            button6.BackColor = Color.WhiteSmoke;
+            label4.Text = "0";
+            pwm = 0;
+        }
+
+        ////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////
+        //------------------> STEP HIZ ARTTIR <-----------------------------
+        private void button8_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("T");
+            button8.BackColor = Color.WhiteSmoke;
+            step = step + 1;
+            if (step >= 5) step = 5;
+            label6.Text = Convert.ToString(step);
+
+        }
+        //------------------> STEP HIZ AZALT <-----------------------------
+        private void button9_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("G");
+            button9.BackColor = Color.WhiteSmoke;
+            step = step - 1;
+            if (step <= 1) step = 1;
+            label6.Text = Convert.ToString(step);
+        }
+        //------------------> STEP SAGA DÖN<-----------------------------
+        private void button10_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("H");
+            button10.BackColor = Color.Red;
+            button11.BackColor = Color.WhiteSmoke;
+        }
+        //------------------> STEP SOLA DÖN <-----------------------------
+        private void button11_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("F");
+            button11.BackColor = Color.Red;
+            button10.BackColor = Color.WhiteSmoke;
+        }
+        //------------------> STEP DUR <-----------------------------
+        private void button12_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("X");
+            button11.BackColor = Color.WhiteSmoke;
+            button10.BackColor = Color.WhiteSmoke;
+            label6.Text = Convert.ToString(step);
+            step = 0;
+        }
+
+
+
+
+
+        //------------ MOUSE DOWN
+        // DC PWM ARTTIR
+        private void button3_MouseDown(object sender, MouseEventArgs e)
+        {
+            button3.BackColor = Color.Turquoise;
+        }
+        // DC PWM AZALT
+        private void button4_MouseDown(object sender, MouseEventArgs e)
+        {
+            button4.BackColor = Color.Turquoise;
+        }
+        // STEP HIZ ARTTIR
+        private void button8_MouseDown(object sender, MouseEventArgs e)
+        {
+            button8.BackColor = Color.Turquoise;
+        }
+        // STEP HIZ AZALT
+        private void button9_MouseDown(object sender, MouseEventArgs e)
+        {
+            button9.BackColor = Color.Turquoise;
+        }
+
+    }
+}
